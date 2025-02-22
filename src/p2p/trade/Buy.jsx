b029d779c336da4da533d8bbf59f0b0050ffa04b
Highscoretech <highscoretechng@gmail.com>
2025-02-22 22:10:09 +0100

    Okau

--- src/p2p/trade/Buy.jsx
+++ src/p2p/trade/Buy.jsx
@@ -24,8 +24,8 @@ export default function Buy() {
     {/* {showModal ? <BuyModal handleCancelMedal={()=> setShowModal(false)}/> : ""} */}
    
     <div className="trade-list__content">
-        hELLO
-    {/* <div className="trade-table__wrapper">
+
+    <div className="trade-table__wrapper">
         <div className="nt-spin-nested-loading trade-table__spin css-7o12g0">
             <div className="ant-spin-container">
                 <div id="guide-step-five" style={{height: "150px"}}></div>
@@ -172,7 +172,7 @@ export default function Buy() {
                 </div>
             </div>
         </div>
-    </div> */}
+    </div>
     </div>
     </>
 
